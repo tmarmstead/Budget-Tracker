b4d634083ccf213a66105676a9d12dd31feb50dc
Tina Armstead <tina.armstead@gmail.com>
2021-03-28 19:06:27 -0400

    Added uri constant in server.js

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
+const uri = process.env.MONGODB_URI;
 
 const PORT = 3000;
 
@@ -27,3 +28,6 @@ app.use(require("./routes/api.js"));
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
 });
+
+
+// mongodb+srv://tmarmstead44:caw4pit2D9VxO5ig@cluster0.gr0tf.mongodb.net/myFirstDatabase?retryWrites=true&w=majority
