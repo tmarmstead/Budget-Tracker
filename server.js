763aa80ca0240d5164c968d21cb55cb5378ef2d0
Tina Armstead <tina.armstead@gmail.com>
2021-03-28 19:07:39 -0400

    Updated uri use in server.js file

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/budget", {
+mongoose.connect(uri, {
   useNewUrlParser: true,
   useFindAndModify: false,
   useUnifiedTopology: true,
