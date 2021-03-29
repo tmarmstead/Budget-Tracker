e1c23d3d304b2f45074d7d495d06c90105794503
Tina Armstead <tina.armstead@gmail.com>
2021-03-29 16:07:38 -0400

    Added process.env to PORT in server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const mongoose = require("mongoose");
 const compression = require("compression");
 const uri = process.env.MONGODB_URI;
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 const app = express();
 
